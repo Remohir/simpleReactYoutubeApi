c0b83efb476f6bdd5681494d555353044cbe6d6f
Ezequiel <contact@ezedeveloper.com>
2017-09-24 14:22:49 +0100

    display video + video list + styling - search functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail"
 
 const API_KEY = "AIzaSyBvDKe0QITTJqfcgVxD949vbeKf6wOzzOE";
 
 // Create a new component. Component should produce HTML
-const App = () => {
-	return <div>
-		<SearchBar />
-	</div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch("Nadie sabe nada");
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		return <div>
+			<SearchBar />
+			<VideoDetail video={this.state.videos[0]}/>
+			<VideoList videos={ this.state.videos } />
+		</div>;
+	}
 }
 
 // Insert the components in the DOM
